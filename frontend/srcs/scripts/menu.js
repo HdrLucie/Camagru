9fc49d129b85d9cf4f7ef62c27b23008d9730927
HdrLucie <hlucie@student.42.fr>
2024-07-10 18:04:18 +0200

    [LUCIE] : modification menu

--- frontend/srcs/scripts/menu.js
+++ frontend/srcs/scripts/menu.js
@@ -1,3 +1,31 @@
-document.getElementById("takePicture").onclick = async function () {
-    window.location.href = '/takePicture'
+window.onload = checkToken;
+
+function checkToken() {
+    console.log("Function check token")
+    const token = localStorage.getItem('token');
+    console.log(token)
+    if (!token) {
+        // alert('No token found. Please login.');
+        window.location.href = '/';
+    }
+}
+
+document.getElementById("burger").onclick = function () {
+    let burger = document.querySelector(".js-burger");
+    let nav = document.querySelector(".js-nav");
+
+    nav.classList.toggle("_active");
+    burger.classList.toggle("_active");
+}
+
+document.getElementById("logout").onclick = async function () {
+    alert('Logout')
+    const token = localStorage.getItem('token')
+    localStorage.clear()
+    window.location.href = '/'
+}
+
+function redirectionPage(path) {
+    alert('Redirection page')
+    window.location.href = path;
 }
