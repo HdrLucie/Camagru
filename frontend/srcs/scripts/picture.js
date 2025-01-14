69a23694d1a29a01e488b8002f9ba8d8742a7bb9
HdrLucie <hlucie@student.42.fr>
2025-01-14 15:10:29 +0100

    [FEATURE] : starting the main feature, take photo and send its to the DB.
    I've started by some front things because it wa too ugly

--- frontend/srcs/scripts/picture.js
+++ frontend/srcs/scripts/picture.js
@@ -15,7 +15,7 @@ function checkToken() {
   // width to the value defined here, but the height will be
   // calculated based on the aspect ratio of the input stream.
 
-  const width = 320; // We will scale the photo width to this
+  const width = 0; // We will scale the photo width to this
   let height = 0; // This will be computed based on the input stream
 
   // |streaming| indicates whether or not we're currently streaming
