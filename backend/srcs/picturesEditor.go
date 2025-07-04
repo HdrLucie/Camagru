88744fe553fe38bf41e244c22932a56158b1859a
HdrLucie <hlucie@student.42.fr>
2025-07-04 17:55:18 +0200

    First step : upload image to database OK

--- backend/srcs/picturesEditor.go
+++ backend/srcs/picturesEditor.go
@@ -1,13 +1,15 @@
-package main 
+package main
 
 import (
 	"fmt"
+	"strconv"
 	// "encoding/hex"
 	// "encoding/json"
 	"net/http"
 )
 
 func (app *App) downloadImage(writer http.ResponseWriter, request *http.Request) {
+	// var image Pictures;
 
 	fmt.Println(Yellow + "Download image" + Reset)
 	writer.Header().Set("Content-Type", "application/json")
@@ -31,7 +33,12 @@ func (app *App) downloadImage(writer http.ResponseWriter, request *http.Request)
     defer file.Close()
 	timeStamp := request.FormValue("timestamp");
 	userId := request.FormValue("id");
+	tmpId, _ := strconv.Atoi(userId);
 	fmt.Println(timeStamp, userId);
-
-	
+	err = app.dataBase.QueryRow("INSERT INTO images (image_path, userId, uploadTime) VALUES ($1, $2, $3) RETURNING id", "path", tmpId, timeStamp).Scan(&userId);
+	if err != nil {
+		fmt.Println(Red + "Error : insert image in DB" + Reset);
+		http.Error(writer, err.Error(), http.StatusInternalServerError);
+		return;
+	}
 }
